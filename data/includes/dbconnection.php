183d1ec2aa6a0f48896dc329e30dd4b40c5fae20
jtacuban <jaketacuban@gmail.com>
2024-12-06 10:23:26 +0800

    Update dbconnection.php

--- data/includes/dbconnection.php
+++ data/includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("mysql", "root", "", "root");
+$con=mysqli_connect("mysql", "root", "root", "zmsdb");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
